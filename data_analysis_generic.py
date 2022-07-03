0ff14a2574e8af14caa892ddc0201804d1e8e759
sanchy32 <78264429+sanchy32@users.noreply.github.com>
2022-07-03 02:28:18 -0400

    Add files via upload
    Generic_code for video data extraction and analysis with graphing. First upload.

---
+++ data_analysis_generic.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 18 13:48:29 2022
+
+Plots for volume and pressure relationship against time relationship
+Data from pressure from previous experiments and data for volume extracted from video frame clickpoints
+Data as used is Current proportional to pressure and Diameters/Lengths proportional to cube root of volume in pixels
+Pixels could be coverted to metric/imperial unit with pixel per meter constant considering diameter of pressure gauge in video is 5in
+
+Edited on Mon Mar 18 14:44 2022, A2.
+Plot drawstyle set to step.
+@author: Godswill Agbofode
+"""
+
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt 
+import sys   # for plotting the images
+#%matplotlib inline
+import pandas as pd
+#from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+from tkinter.filedialog import askopenfilename
+video_name = askopenfilename()
+# =============================================================================
+# d_p= pd.read_excel("D:\\Documents\\Documents\\McGill School Documents\\1.)Winter_2022\\MECH 403_ THESIS\\20200309-1354-vvb3_for_python_a1.xlsx")
+# d_p.head()
+#=============================================================================
+#Headings: 'Time(ms)' , 'Current_0' , 'Current_1'
+#print(d_p['Time(ms)'])
+
+
+#%%Parameters
+#filename = input("Enter excel file name")
+SFac = 5 #Initial width diameter of bubble in cm
+#d_v=pd.read_excel("D:\\Documents\\Documents\\McGill School Documents\\1.)Winter_2022\\MECH 403_ THESIS\\20200203_VCchamber_Spinchpoints-2.xlsx")
+d_v=pd.read_excel(video_name)
+SFac = SFac/(d_v['D1'].iloc[1])
+d_v.head()
+#Headings: 'Time(in milliseconds)' , 'Dh1' , 'Dv1, 'Dh2' , 'Dv2', 'Dh3' , 'Dv3'
+
+#%%
+# =============================================================================
+# fig, ax = plt.subplots()
+# ax.set_xlabel(r'$t$ (s)', fontsize='12')
+# ax.set_ylabel(r'$Diameter\;(cm)$', fontsize='12')
+# ax.plot(d_v['Time(in milliseconds)']/1000, (d_v['Dh1'])*SFac, drawstyle = 'steps-post', label='$Dh1$', color='black')
+# ax.plot(d_v['Time(in milliseconds)']/1000, (d_v['Dh2'])*SFac, drawstyle = 'steps-post', label='$Dh2$', color='red')
+# ax.plot(d_v['Time(in milliseconds)']/1000, (d_v['Dh3'])*SFac, drawstyle = 'steps-post', label='$Dh3$', color='blue')
+# lines, labels = ax.get_legend_handles_labels() 
+# ax.legend(lines, labels, loc='upper left', fontsize ='12')
+# fig.tight_layout()
+# plt.show()
+# fig.savefig('20200203_VCchamber_Spinchpoints-2_Hor_D.jpeg', format='pdf')
+# =============================================================================
+
+#plt.xlim(240, 420)
+#plt.ylim(-25, 15)
+# Plot data
+#Plot Pressures/Currents
+
+#%%
+#ax2 = ax.twinx()
+#ax.set_ylabel(r'$Volume\;(Proportional\;to\;Pixels\;(CentiPixels))$', fontsize = '12')#', $Volume$, $Scaled to fit (units)'
+
+
+fig, ax2 = plt.subplots()
+ax2.set_xlabel(r'$t$ (s)', fontsize='12')
+ax2.set_ylabel(r'$Diameter\;(pi)$', fontsize='12')
+ax2.plot(d_v['Time(in milliseconds)']/1000, (d_v['D1'])*SFac, drawstyle = 'steps-post', label='$D1$', color='black')
+ax2.plot(d_v['Time(in milliseconds)']/1000, (d_v['D2'])*SFac, drawstyle = 'steps-post', label='$D2$', color='red')
+ax2.plot(d_v['Time(in milliseconds)']/1000, (d_v['D3'])*SFac, drawstyle = 'steps-post', label='$D3$', color='blue')
+lines, labels = ax2.get_legend_handles_labels() 
+ax2.legend(lines, labels, loc='upper left', fontsize ='12')
+fig.tight_layout()
+plt.show()
+fig.savefig(filename + 'pdf', format='pdf')
+
+
---
+++ video_data_extract_sample_generic.py
@@ -0,0 +1,138 @@
+"""
+Created on Fri Feb 11 15:14:18 2022
+
+@author: Godswill Agbofode
+Reference: To be added
+Edit to sync frames with those from dated data on pressure
+    """
+#Imports
+import cv2     # for capturing videos
+import math   # for mathematical operations
+import matplotlib.pyplot as plt 
+import sys   # for plotting the images
+#%matplotlib inline
+import pandas as pd
+#from keras.preprocessing import image   # for preprocessing the images
+import numpy as np    # for mathematical operations
+#from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+#Tk().withdraw()
+video_name = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+#print(filename)
+#from keras.utils import np_utils
+#from skimage.transform import resize   # for resizing images
+#send output to textfile
+#video_name = input("Enter video file name")
+#video_name = "20200203_VCchamber_Spinchpoints-2.mp4" #video_name + "_data.txt"
+#'Insert code to check that '
+#%%Parameters
+#Data column headers template: Frame Count, Frame ID, Time(in milliseconds), E1_Dh1_(x,y), E1_Dh2_(x,y), E1_Dv1_(x,y), E1_Dv2_(x,y), E2_Dh1_(x,y), E2_Dh2_(x,y), E2_Dh1_(x,y), E2_Dh2_(x,y), E3_Dh1_(x,y), E3_Dh2_(x,y), E3_Dh1_(x,y), E3_Dh2_(x,y)
+headers = 'Frame Count, Frame ID, Time(in milliseconds), E1_Dh1_(x,y), E1_Dh2_(x,y), E2_Dh1_(x,y), E2_Dh2_(x,y), E3_Dh1_(x,y), E3_Dh2_(x,y)'
+record_start = input("Enter start time in seconds of recorded measurements: ")
+record_start = int(record_start)
+print("Start time :" + str(record_start)) 
+record_end = input("Enter end time in seconds of recorded measurements: ")
+record_end = int(record_end)
+print("End time :" + str(record_end))
+myFrameRate = 0 #placeholder for assignment on line 78 after video is chosen
+#%%Introduce clicking functionality
+def click_event(event, x, y, flags, params):
+ 
+    # checking for left mouse clicks
+    if event == cv2.EVENT_LBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        #print(str(count)+ ', '+ str(cap.get(0))+', ' + str(x), ', ', str(y),)
+        print(str(x)+','+str(y)+ ', ', end = '')
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(img, str(x) + ',' +
+                    str(y), (x,y), font,
+                    1, (255, 0, 0), 2)
+        cv2.imshow('image', img)
+ 
+    # checking for right mouse clicks    
+    if event==cv2.EVENT_RBUTTONDOWN:
+ 
+        # displaying the coordinates
+        # on the Shell
+        print(x, ' ', y)
+ 
+        # displaying the coordinates
+        # on the image window
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        b = img[y, x, 0]
+        g = img[y, x, 1]
+        r = img[y, x, 2]
+        cv2.putText(img, str(b) + ',' +
+                    str(g) + ',' + str(r),
+                    (x,y), font, 1,
+                    (255, 255, 0), 2)
+        cv2.imshow('image', img)
+#%%Read the video, extract frames from it and save 
+# them as images
+count = 0
+#videoFile = "D:\Documents\Documents\McGill School Documents\1.)Winter_2022\MECH 513\Sequential auxetics video_v02.m4v"
+cap = cv2.VideoCapture(video_name)   # capturing the video from the given path
+frameRate = round(cap.get(5))
+print( "The video frameRate = " + str(cap.get(5)) +" approximately "+ str(frameRate)) #frame rate
+myFrameRate = input("Enter desired frames per second for measurements: ")
+myFrameRate = int(myFrameRate)
+frameIDDivisor = math.floor(frameRate//myFrameRate)
+#print("The framerate is " + str(frameRate))
+#print("The cap is opened: " + str(cap.isOpened()))
+#%%
+sys.stdout = open(video_name + ".txt", "w")
+print(headers, end = '')
+frameId =cap.get(1)
+#print(frameId)
+#%%
+while(True):
+    #print('hello')
+    frameId = cap.get(1) #current frame number
+    #%%
+    ret, frame = cap.read()
+    if (ret != True):
+        #print("Is not  ")
+        break
+    #print('Bye')
+    if(cap.get(0)<record_start*1000 or cap.get(0)>(record_end*1000)):
+        #print('Time in ms: ' + str(cap.get(0)) +', Frame ID: ' + str(frameId) + ', Rel Pos: '+ str(cap.get(2)))
+        continue
+    #if ((frameId -3975)%3 == 0):
+    if (frameId%frameIDDivisor==0):    
+            #filename ="" % count
+            #print('Stuff happens')
+            #print('Time in ms: ' + str(cap.get(0)) +', Frame ID: ' + str(frameId) + ', Stuff happens')
+           
+            ###Introduce clicking 
+           print('') 
+           print(str(count) + ',' + str(frameId)+',' + str(cap.get(0))+', ', end = '') 
+           if __name__=="__main__":
+             
+                # reading the image
+                #img = cv2.imread(frame)
+                img = frame 
+                # displaying the image
+                cv2.imshow('image', img)
+             
+                # setting mouse handler for the image
+                # and calling the click_event() function
+                cv2.setMouseCallback('image', click_event)
+             
+                # wait for a key to be pressed to exit
+                cv2.waitKey(0)
+             
+                # close the window
+                cv2.destroyAllWindows()
+            #cv2.imwrite(f'frame_{count}.jpg', frame)
+                count+=1
+cap.release()
+sys.stdout.close()
+print ("Done!")
+
+#%%
+#img = plt.imread('frame_28.jpg')   # reading image using its name
+#plt.imshow(img)
